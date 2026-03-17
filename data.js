fcfd91a0793c91c006915b939c1a4f239c922214
Hirosuke Nishi Grohmann <97203481+henrigrohmann@users.noreply.github.com>
2026-03-17 19:37:57 +0900

    data.js を更新

--- data.js
+++ data.js
@@ -1,9 +1,20 @@
 // @ts-nocheck
 console.log("data.js loaded");
 
+// Live Server の URL → 5500
+// API の URL → 8001 に自動変換
+const API_BASE = window.location.origin.replace("5500", "8001");
+
 window.api = {
   async getScatterData() {
-    const res = await fetch("http://localhost:8001/scatter_data");
-    return res.json();
+    const url = `${API_BASE}/scatter_data`;
+    console.log("FETCH:", url);
+
+    const res = await fetch(url);
+    console.log("STATUS:", res.status);
+
+    const json = await res.json();
+    console.log("JSON:", json);
+    return json;
   }
 };
